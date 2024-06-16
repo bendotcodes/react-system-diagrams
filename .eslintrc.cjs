92d996bbca3d58c4684c920e7f5cf796bf2a292e
Benoit Tremblay <trembl.ben@gmail.com>
2024-06-16 12:14:52 -0400

    Add storybook (#3)
    * Add storybook
    
    * Fix Storybook on GH pages
    
    * Disable jekyll only on main deployment

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,34 +1,25 @@
 /* eslint-env node */
 module.exports = {
+  root: true,
   extends: [
     'eslint:recommended',
+    'plugin:@typescript-eslint/strict-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
+    'plugin:storybook/recommended',
     'prettier',
   ],
-  root: true,
+  plugins: ['@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
   settings: {
     react: {
       version: 'detect',
     },
   },
-  overrides: [
-    {
-      files: ['src/**/*.{ts,tsx}'],
-      extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/strict-type-checked',
-        'plugin:@typescript-eslint/stylistic-type-checked',
-        'plugin:react/recommended',
-        'plugin:react/jsx-runtime',
-        'prettier',
-      ],
-      parser: '@typescript-eslint/parser',
-      parserOptions: {
-        project: true,
-        tsconfigRootDir: __dirname,
-      },
-      plugins: ['@typescript-eslint'],
-    },
-  ],
+  ignorePatterns: ['!.*', 'dist', 'node_modules', 'storybook-static'],
 };
